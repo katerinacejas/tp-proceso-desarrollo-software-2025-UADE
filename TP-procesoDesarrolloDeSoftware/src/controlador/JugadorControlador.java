a675c769d505210c3656de112d8933362e6dd081
katerinacejas <katerinacejas3@gmail.com>
2025-05-28 03:23:23 -0300

    inicio controlador dao y dto de jugador

---
+++ TP-procesoDesarrolloDeSoftware/src/controlador/JugadorControlador.java
@@ -0,0 +1,9 @@
+package controlador;
+
+import modelo.entidad.jugador.Jugador;
+import vista.VistaPrincipal;
+
+public class JugadorControlador {
+    private Jugador jugador;
+    private VistaPrincipal vistaPrincipal;
+}
