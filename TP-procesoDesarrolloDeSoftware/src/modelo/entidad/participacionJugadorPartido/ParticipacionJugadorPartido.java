b7f53ea8bf28455fb7e14555eeb26d273f635606
Diego B <dbari@seidoranalytics.com>
2025-06-08 21:23:23 -0300

    implementacion State
    Co-Authored-By: mflorbird <178087774+mflorbird@users.noreply.github.com>

--- TP-procesoDesarrolloDeSoftware/src/modelo/entidad/participacionJugadorPartido/ParticipacionJugadorPartido.java
+++ TP-procesoDesarrolloDeSoftware/src/modelo/entidad/participacionJugadorPartido/ParticipacionJugadorPartido.java
@@ -1,15 +1,48 @@
 package modelo.entidad.participacionJugadorPartido;
 
 import modelo.entidad.jugador.Jugador;
-import modelo.entidad.partido.Partido;
 import modelo.enumerador.EstadoParticipacion;
 
 public class ParticipacionJugadorPartido {
     Jugador jugador;
-    Partido partido;
     EstadoParticipacion confirmacion;
 
-    public void actualizarParticipacion(EstadoParticipacion estado){
-        this.confirmacion = estado;
+    public ParticipacionJugadorPartido(Jugador jugador) {
+        if (jugador == null) {
+            throw new IllegalArgumentException("El jugador no puede ser null");
+        }
+        this.jugador = jugador;
+        this.confirmacion = EstadoParticipacion.PENDIENTE; // Por defecto, la participación está pendiente
     }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+    public EstadoParticipacion getConfirmacion() {
+        return confirmacion;
+    }
+
+    public void aceptarParticipacion() {
+        this.confirmacion = EstadoParticipacion.ACEPTADO;
+    }
+
+    public void rechazarParticipacion() {
+        this.confirmacion = EstadoParticipacion.RECHAZADO;
+    }
+
+    public boolean isAceptado() {
+        return this.confirmacion == EstadoParticipacion.ACEPTADO;
+    }
+
+    public boolean isPendiente() {
+        return this.confirmacion == EstadoParticipacion.PENDIENTE;
+    }
+
+    public boolean isRechazado() {
+        return this.confirmacion == EstadoParticipacion.RECHAZADO;
+    }
+
+
+
+
 }
