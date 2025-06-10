830dd0c89a18c0cedfab2520c762c27e7c116529
katerinacejas <katerinacejas3@gmail.com>
2025-06-10 18:42:20 -0300

    controller, base y DAO de Deporte para create y getById

--- TP-procesoDesarrolloDeSoftware/src/modelo/entidad/deporte/Deporte.java
+++ TP-procesoDesarrolloDeSoftware/src/modelo/entidad/deporte/Deporte.java
@@ -1,10 +1,39 @@
 package modelo.entidad.deporte;
 
+import modelo.dao.DeporteDAO;
+
 public class Deporte {
     private String nombre;
     private int cantJugadores;
 
-    public int getCantJugadores() {
-        return this.cantJugadores;
+    // metodos de que se llaman en el controller
+    public void createDeporte(Deporte deporte) {
+        DeporteDAO deporteDAO = new DeporteDAO();
+        deporteDAO.createDeporte(deporte);
     }
+
+    public Deporte getDeporteById(int id){
+        DeporteDAO deporteDAO = new DeporteDAO();
+        return deporteDAO.getDeporteById(id);
+    }
+
+
+    // setters y getters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantJugadores() {
+        return cantJugadores;
+    }
+
+    public void setCantJugadores(int cantJugadores) {
+        this.cantJugadores = cantJugadores;
+    }
+
 }
