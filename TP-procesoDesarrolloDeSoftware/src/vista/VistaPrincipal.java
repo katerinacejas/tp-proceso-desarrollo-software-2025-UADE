be200d006a0a4401a46b76d42e740b9423410fc5
katerinacejas <katerinacejas3@gmail.com>
2025-06-14 18:48:03 -0300

    vistaaaaaaaa

--- TP-procesoDesarrolloDeSoftware/src/vista/VistaPrincipal.java
+++ TP-procesoDesarrolloDeSoftware/src/vista/VistaPrincipal.java
@@ -153,20 +153,3 @@ public class VistaPrincipal {
         System.out.println(jugadorDTO.getId());
     }
 }
-
-/*
-DeporteController dController = new DeporteController();
-        DeporteDTO volleyDTO = new DeporteDTO();
-        volleyDTO.setNombre("VOLLEY");
-        volleyDTO.setCantJugadores(12);
-        dController.createDeporte(volleyDTO);
-        JugadorController jController = new JugadorController();
-        JugadorDTO jugadorDTO = new JugadorDTO();
-        jugadorDTO.setNombreUsuario("jRodriguez");
-        jugadorDTO.setDeportesFavoritos(Arrays.asList(volleyDTO.getId()));
-        jController.createJugador(jugadorDTO);
-        JugadorDTO jugadorDTO2 = new JugadorDTO();
-        jugadorDTO2.setNombreUsuario("kCejas");
-        jugadorDTO2.setDeportesFavoritos(Arrays.asList(volleyDTO.getId()));
-        jController.createJugador(jugadorDTO2);
- */
