1701d8f5d90b5cb38a40fdbee50289774993549e
julirodriguez <juliarodriguez@uade.edu.ar>
2025-06-16 12:28:51 -0300

    Observers + Notifications + Adapters

--- TP-procesoDesarrolloDeSoftware/src/modelo/adapter/AdapterEmailJavaMail.java
+++ TP-procesoDesarrolloDeSoftware/src/modelo/adapter/AdapterEmailJavaMail.java
@@ -1,10 +1,14 @@
 package modelo.adapter;
 
+import java.util.List;
+
 import modelo.entidad.notificacion.Notificacion;
 
 public class AdapterEmailJavaMail implements IAdapterEmail{
     @Override
-    public void enviarEmail(Notificacion notificacion){
-
+    public void enviarEmails(List<Notificacion> notificaciones){
+        for (Notificacion notificacionEmail : notificaciones){
+            System.out.println(notificacionEmail.getMensaje());
+        }
     }
 }
--- TP-procesoDesarrolloDeSoftware/src/modelo/strategy/notificacion/NotificacionEmail.java
+++ TP-procesoDesarrolloDeSoftware/src/modelo/strategy/notificacion/NotificacionEmail.java
@@ -1,5 +1,6 @@
 package modelo.strategy.notificacion;
 
+import modelo.adapter.AdapterEmailJavaMail;
 import modelo.adapter.IAdapterEmail;
 import modelo.entidad.notificacion.Notificacion;
 
@@ -8,8 +9,13 @@ import java.util.List;
 public class NotificacionEmail implements IServicioNotificacion{
     IAdapterEmail adapter;
 
+    public NotificacionEmail() {
+        this.adapter = new AdapterEmailJavaMail();
+    }
+
     @Override
     public void notificar(List<Notificacion> notificaciones) {
+            adapter.enviarEmails(notificaciones);
 
     }
 }
